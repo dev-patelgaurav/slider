987bfac7c867b672bc994d24c33c710d71b1a915
dev-patelgaurav <dev.patelgaruav@gmail.com>
2025-02-03 10:05:37 +0530

    initial setup with some html files

---
+++ assets/js/counter.js
@@ -0,0 +1,36 @@
+$.fn.jQuerySimpleCounter = function (options) {
+    var settings = $.extend({
+        start: 0,
+        end: 100,
+        easing: 'swing',
+        duration: 400,
+        complete: ''
+    }, options);
+
+    var thisElement = $(this);
+
+    $({ count: settings.start }).animate({ count: settings.end }, {
+        duration: settings.duration,
+        easing: settings.easing,
+        step: function () {
+            var mathCount = Math.ceil(this.count);
+            thisElement.text(mathCount);
+        },
+        complete: settings.complete
+    });
+};
+
+
+$('#n1').jQuerySimpleCounter({ end: 55, duration: 3000 });
+$('#n2').jQuerySimpleCounter({ end: 24, duration: 3000 });
+$('#n3').jQuerySimpleCounter({ end: 359, duration: 3000 });
+$('#n4').jQuerySimpleCounter({ end: 246, duration: 3000 });
+
+
+
+/* AUTHOR LINK */
+$('.about-me-img').hover(function () {
+    $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+}, function () {
+    $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+});
